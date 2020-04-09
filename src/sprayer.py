b2d5a8b2dd47fd7be07dcda389b9fd4782465e65
chianugoogidi <ogidif@Franklin.localdomain>
2020-04-08 19:58:34 -0500

    Updated for compatibility with the latest JCA Controller API

--- src/sprayer.py
+++ src/sprayer.py
@@ -6,7 +6,7 @@
 #
 # Description:
 #   The ROS node in charge of activating spray nozzles based on input from
-#   object detection model.
+#   YOLO object detection model.
 #
 # History:
 #   2019-03-15 by Franklin ogidi
@@ -17,151 +17,212 @@
 #   - Added y-axis of bounding box for spray time delay
 #   2019-04-22 by Franklin Ogidi
 #   - Added threading
+#   2019-07-16 by Franklin Ogidi
+#   - Changed code structure to include Sprayer class
+#   - Modified code to publish nozzle state as a Bool so that it can be
+#     sent to a web server for use on an app
+#   2020-03-07 by Franklin Ogidi
+#   - Updated for compatibility with the latest JCA Controller API
+#	http://docs.jcaelectronics.ca/jcaroscontroller/ref-falcon/
 # *****************************************************************************
 
 import time
 import rospy
 import threading
-from std_msgs.msg import Int64
-from std_msgs.msg import Float64
-from falcon_ros_node.msg import falcon_output
+
 from darknet_ros_msgs.msg import BoundingBoxes
+from std_msgs.msg import Float64, Int64, Bool, String
+from jca_controller_ros_nodes_interface.msg import Output
 
-def turn_on_pump():
-    """
-    Turns on the pump once the script is on. Pump stays on until node is shutdown.
-    """
+class Sprayer:
 	
-    msg = falcon_output()
-    msg.header.stamp = rospy.Time.now()
-    msg.header.frame_id = "pump is on"
-    msg.value = float(100)
-    pub = rospy.Publisher('/falcon/outputs/hso_1', falcon_output, queue_size=1, latch = True)
-    pub.publish(msg)
+    def __init__(self):
+	self.image_width = 640 	#px
+	self.image_height = 480 #px
 
-def get_vehicle_speed(msg):
-    """
-    Gets vehicle speed (in km/hr) from capstone_ros package. Converts units to m/s.
-    """
+	self.speed = 0.0
+	self.foxtail_counter = 0
+	self.cocklebur_counter = 0
+	self.healthy_corn_counter = 0
+	self.diseased_corn_counter = 0
+	self.giant_ragweed_counter = 0
+
+	# Nozzle positions
+	self.nozzle_1_position = 0
+	self.nozzle_2_position = 0
+	self.nozzle_3_position = 0
+	self.nozzle_4_position = 0
+	self.nozzle_5_position = 0
+	self.nozzle_6_position = 0
 	
-    #rospy.loginfo(msg)
+	# falcon publisher
+	self.falcon_pub = rospy.Publisher('/falcon1/command/output', Output, queue_size=10, latch=True)
+
+	# count publishers
+	self.diseased_corn_count = rospy.Publisher("dieseased_corn_count", Int64, queue_size=1, latch=True)
+	self.giant_ragweed_count = rospy.Publisher("giant_ragweed_count", Int64, queue_size=1, latch=True)
+	self.healthy_corn_count = rospy.Publisher("healthy_corn_count", Int64, queue_size=1, latch=True)
+	self.cocklebur_count = rospy.Publisher("cocklebur_count", Int64, queue_size=1, latch=True)
+	self.foxtail_count = rospy.Publisher("foxtail_count", Int64, queue_size=1, latch=True)
+
+    def turn_on_pump(self):
+	"""
+	Turns on the pump once the script is started. Pump stays on until node is shutdown.
+	"""
+
+	msg = Output()
+	msg.name = 'hso_1'
+	msg.value = 100
+	self.falcon_pub.publish(msg)
 	
-    global speed
-    speed = 0.277778 * msg.data #m/s
+    def get_vehicle_speed(self, msg):
+	"""
+	Gets vehicle speed (in km/hr) from capstone_ros package. Converts units to m/s.
+	Reference: https://github.com/rahmant3/capstoneROS2018_public
+	"""
+
+	#rospy.loginfo(msg)
+	self.speed = 0.277778 * msg.data #m/s
+		
+    def set_boom_parameters(self):
+	#ATV Boom parameters
+	nozzle6_range = 50 			# 50 cm
+	nozzle5_range = 60 			# 60 cm
+	nozzle4_range = nozzle5_range + 50 	# 110 cm
+	nozzle3_range = nozzle4_range + 50 	# 160 cm
+	nozzle2_range = nozzle3_range + 50 	# 210 cm
+	nozzle1_range = nozzle3_range + 60 	# 220 cm
+	boom_length = nozzle1_range 		# 220 cm
+
+	#normalize boom parameters by image width
+	self.nozzle_1_position = float(nozzle1_range) / boom_length * self.image_width	# 640 px
+	self.nozzle_2_position = float(nozzle2_range) / boom_length * self.image_width	# 611 px
+	self.nozzle_3_position = float(nozzle3_range) / boom_length * self.image_width	# 465 px
+	self.nozzle_4_position = float(nozzle4_range) / boom_length * self.image_width  # 320 px
+	self.nozzle_5_position = float(nozzle5_range) / boom_length * self.image_width	# 175 px
+	self.nozzle_6_position = float(nozzle6_range) / boom_length * self.image_width	# 145 px
     
-def process_bounding_box(msg):
-    """
-    Gets array of bounding boxes from darknet_ros package. Determines nozzle to activate.
-    """
-    #rospy.loginfo(msg)
+    def process_bounding_box(self, msg):
+	"""
+	Gets array of bounding boxes from darknet_ros package. Determines nozzle to activate.
+	Reference: https://github.com/leggedrobotics/darknet_ros
+	"""
+	#rospy.loginfo(msg)
 
-    global speed
-    global weed_counter
-    global corn_counter
-    output_pin = ""
-    image_width = 640 	#px
-    image_height = 480 	#px
-    
-    #ATV Boom parameters
-    nozzle6_range = 50 			# 50 cm
-    nozzle5_range = 60 			# 60 cm
-    nozzle4_range = nozzle5_range + 50 	# 110 cm
-    nozzle3_range = nozzle4_range + 50 	# 160 cm
-    nozzle2_range = nozzle3_range + 50 	# 210 cm
-    nozzle1_range = nozzle3_range + 60 	# 220 cm
-    boom_length = nozzle1_range 	# 220 cm
+	bounding_box = msg.bounding_boxes
 
-    #normalize boom parameters by image width
-    s1 = float(nozzle1_range) / boom_length * image_width	# 640 px
-    s2 = float(nozzle2_range) / boom_length * image_width	# 611 px
-    s3 = float(nozzle3_range) / boom_length * image_width	# 465 px
-    s4 = float(nozzle4_range) / boom_length * image_width   	# 320 px
-    s5 = float(nozzle5_range) / boom_length * image_width	# 175 px
-    s6 = float(nozzle6_range) / boom_length * image_width	# 145 px
-    
-    bounding_box = msg.bounding_boxes
-    
-    for i in range(len(bounding_box)):
-        obj = bounding_box[i].Class
-        xmin = bounding_box[i].xmin
-        ymin = bounding_box[i].ymin
-        xmax = bounding_box[i].xmax
-        ymax = bounding_box[i].ymax
-        #prob = bounding_box[i].probability
+	for i in range(len(bounding_box)):
+  	    obj = bounding_box[i].Class
+	    #prob = bounding_box[i].probability
 
-        #find center of bounding box
-        xmid = xmin + ((xmax - xmin) * 0.5)
-        ymid = ymin + ((ymax - ymin) * 0.5)
-        
-        if obj == "corn (healthy)" or obj == "corn (diseased)":
-  	    corn_counter += 1
-	    corn_count.publish(corn_counter)
-            if xmid >= s3:
-                output_pin = "hso_8" #solenoid 2
-            elif xmid <= s5:
-                output_pin = "hso_6" #solenoid 5
-        else:
-	    weed_counter += 1
-	    weed_count.publish(weed_counter)
-            if xmid <= s6:
-                output_pin = "hso_3" #solenoid 6
-            elif xmid <= s4 and xmid > s6:
-                output_pin = "hso_10" #solenoid 4
-            elif xmid <= s3 and xmid > s4:
-                output_pin = "hso_7" #solenoid 3
-            elif xmid <= s1 and xmid > s4:
-                output_pin = "hso_5" #solenoid 1
-	distance = 4 + ((float(ymid) / image_height) * 1.60) #meters
+	    if self.speed == 0:
+		time_to_nozzle = 0
+		print("Detected "+obj+"; will not spray unless vehicle is moving")
+	    else:
+		xmin = bounding_box[i].xmin
+		ymin = bounding_box[i].ymin
+		xmax = bounding_box[i].xmax
+		ymax = bounding_box[i].ymax
+				
+		#find center of bounding box
+		xmid = xmin + ((xmax - xmin) * 0.5)
+		ymid = ymin + ((ymax - ymin) * 0.5)
 
-        if speed == 0:
-            time_to_nozzle = 0
-        else:
-            time_to_nozzle = float(distance) / speed
-	
-        threading.Timer(time_to_nozzle, activate_nozzle, args=[output_pin]).start()
-	
-def activate_nozzle(output_pin):
-    """
-    Activates a nozzle for a specified duration of time.
-    """
+		distance = 4 + ((float(ymid) / self.image_height) * 1.60) #meters
+		time_to_nozzle = float(distance) / self.speed
 
-    pub = rospy.Publisher("/falcon/outputs/"+output_pin, falcon_output, queue_size=10, latch = True)
+		if obj == "corn (healthy)":
+		    self.healthy_corn_counter += 1
+		    output_pin = self.select_corn_nozzle(xmid)
+		    self.healthy_corn_count.publish(self.healthy_corn_counter)
+		elif obj == "corn (diseased)":
+		    self.diseased_corn_counter += 1
+		    output_pin = self.select_corn_nozzle(xmid)
+		    self.diseased_corn_count.publish(self.diseased_corn_counter)
+		elif obj == "giant ragweed":
+		    self.giant_ragweed_counter += 1
+		    output_pin = self.select_weed_nozzle(xmid)
+		    self.giant_ragweed_count.publish(self.giant_ragweed_counter)
+		elif obj == "foxtail":
+		    self.foxtail_counter += 1
+		    output_pin = self.select_weed_nozzle(xmid)
+		    self.foxtail_count.publish(self.foxtail_counter)
+		elif obj == "cocklebur":
+		    self.cocklebur_counter += 1	
+		    output_pin = self.select_weed_nozzle(xmid)
+		    self.cocklebur_count.publish(self.cocklebur_counter)
+		else:
+		    rospy.loginfo(obj+" is not a valid object!")
+
+		threading.Timer(time_to_nozzle, self.activate_nozzle, args=[output_pin]).start()
+				
+    def select_corn_nozzle(self, xmid):
+	if xmid >= self.nozzle_3_position:
+	    output_pin = "hso_8" # nozzle 2
+	elif xmid <= self.nozzle_5_position:
+	    output_pin = "hso_6" # nozzle 5
+	else:
+	    rospy.loginfo("Output pin could not be determined")
+	    output_pin = ""
+			
+	return output_pin
 	
-    falcon_msg = falcon_output()
-    falcon_msg.header.stamp = rospy.Time.now()
-    falcon_msg.header.frame_id = "sprayed "+output_pin
-    falcon_msg.value = float(100)
-    pub.publish(falcon_msg)
+    def select_weed_nozzle(self, xmid):
+	if xmid <= self.nozzle_6_position:
+  	    output_pin = "hso_3" # nozzle 6
+	elif xmid <= self.nozzle_4_position and xmid > self.nozzle_6_position:
+	    output_pin = "hso_10" # nozzle 4
+	elif xmid <= self.nozzle_3_position and xmid > self.nozzle_4_position:
+	    output_pin = "hso_7" # nozzle 3
+	elif xmid <= self.nozzle_1_position and xmid > self.nozzle_4_position:
+	    output_pin = "hso_5" # nozzle 1
+	else:
+	    rospy.loginfo("Output pin could not be determined")
+	    output_pin = ""
+			
+	return output_pin
 	
-    spray_duration = 0.25 #s
+    def activate_nozzle(self, output_pin):
+	"""
+	Activates a nozzle for a specified duration of time.
+	"""
+	if output_pin != "":
+	    falcon_msg = Output()
+	    falcon_msg.name = output_pin
+	    falcon_msg.value = float(100)
+	    self.falcon_pub.publish(falcon_msg)
+
+	    spray_duration = 0.25 #s
+
+	    falcon_msg.value = float(0)
+	    time.sleep(spray_duration)
+	    self.falcon_pub.publish(falcon_msg)
+
+	else:
+	    rospy.loginfo("No nozzle was activated because output pin could not be determined")
 	
-    falcon_msg.header.stamp = rospy.Time.now()
-    falcon_msg.header.frame_id = output_pin+" off"
-    falcon_msg.value = float(0)
-    time.sleep(spray_duration)
-    pub.publish(falcon_msg)
-	
-def turn_off_pump():
-    """
-    Turns off pump.
-    """
-    pub = rospy.Publisher("/falcon/outputs/hso_1", falcon_output, queue_size=1, latch = True)
-    msg = falcon_output()
-    msg.header.frame_id = "pump is off"
-    msg.value = float(0)
-    pub.publish(msg)
+    def turn_off_pump(self):
+	"""
+	Turns off pump.
+	"""
+
+	msg = Output()
+	msg.name = 'hso_1'
+	msg.value = 0
+	self.falcon_pub.publish(msg)
 
 if __name__ == "__main__":
     try:
         rospy.init_node("sprayer")
-        weed_counter = 0
-	corn_counter = 0
-	turn_on_pump()
-        weed_count = rospy.Publisher("weed_count", Int64, queue_size=1, latch=True)
-	corn_count = rospy.Publisher("corn_count", Int64, queue_size=1, latch=True)
-	rospy.Subscriber("vehicle_speed", Float64, get_vehicle_speed)
-	rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, process_bounding_box)
-        rospy.on_shutdown(turn_off_pump)
+		
+	sprayer_manager = Sprayer()
+	sprayer_manager.turn_on_pump()
+	sprayer_manager.set_boom_parameters()
+	
+	rospy.Subscriber("vehicle_speed", Float64, sprayer_manager.get_vehicle_speed)
+	rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, sprayer_manager.process_bounding_box)
+		
+        rospy.on_shutdown(sprayer_manager.turn_off_pump)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
